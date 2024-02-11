b0b6b9c3e3b1bc0eef073ed48147669fcc10b007
Jobe Moraes <jobecamera@gmail.com>
2024-02-11 18:13:56 -0300

    refactor: aninhamento de urls

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def blog(request):
     return HttpResponse('Blog')
+  
+def exemplo(request):
+    return HttpResponse('Exemplo do blog')
