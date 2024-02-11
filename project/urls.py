feaa8115f76b51b10640db26fffbaec8a4905f54
Jobe Moraes <jobecamera@gmail.com>
2024-02-11 18:06:09 -0300

    feat: criacao app Home e app Blog e path das urls

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home import views as home_views
+from blog import views as blog_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_views.home),
+    path('blog/', blog_views.blog),
 ]
