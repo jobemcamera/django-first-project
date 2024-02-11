4c6c7178d6fcd47a0083117445d5604782ff3a1a
Jobe Moraes <jobecamera@gmail.com>
2024-02-11 18:45:32 -0300

    feat: render templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def blog(request):
-    return HttpResponse('Blog')
-  
+    return render(request, 'blog/index.html')
+
 def exemplo(request):
-    return HttpResponse('Exemplo do blog')
+    return render(request, 'blog/exemplo.html')
