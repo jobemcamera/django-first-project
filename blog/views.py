f391bff55263ef57b3bd31bfea0550cb7882b12e
Jobe Moraes <jobecamera@gmail.com>
2024-02-14 19:15:56 -0300

    fix: error404

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from blog.data import posts
 from typing import Any
-from django.http import HttpRequest
+from django.http import HttpRequest, Http404
 
 # Create your views here.
 
@@ -24,9 +24,8 @@ def post(request: HttpRequest, post_id: int):
             break
 
     if found_post is None:
-        raise Exception('Post não existe')
+        raise Http404('Post não existe')
     
-    print(found_post)
 
     context = {
         'title': 'Post ' + str(found_post['id']),
